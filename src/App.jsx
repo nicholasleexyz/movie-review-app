06ccf81cb382cf4f73321933225135cd59cfccae
Nicholas Lee <nicholasleexyz@gmail.com>
2023-08-31 06:46:59 -0600

    movie poster bar styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,17 @@ function App() {
 
   return (
     <>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, omnis mollitia blanditiis ad beatae magni rem, nemo vel accusamus voluptas maiores ipsa alias facilis deserunt. Blanditiis autem repellendus inventore fugiat?</p >
+      <h1 className='page-title'>Movie Reviews</h1>
       <div className="movies">
         <div className="box">
           {movies.Search.map((movie, i) => <img className='poster' key={i} src={movie.Poster}></img>)}
         </div>
       </div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, omnis mollitia blanditiis ad beatae magni rem, nemo vel accusamus voluptas maiores ipsa alias facilis deserunt. Blanditiis autem repellendus inventore fugiat?</p >
+      <div className="content">
+        <button>asdf</button>
+        <button>asdf</button>
+        <button>asdf</button>
+      </div>
     </>
   )
 }
