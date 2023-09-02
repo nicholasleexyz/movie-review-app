0c6b226026fd1d0d089c51453456c0ed68c635bc
Nicholas Lee <nicholasleexyz@gmail.com>
2023-09-01 19:31:49 -0600

    layout changes for 1080 resolutions

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,25 @@ const movies = await fetch('/movies.json').then(response => response.json())
 
 // const currentMovieData = await getData();
 function App() {
-// i=tt4154796&apikey=c8b52503
+  // i=tt4154796&apikey=c8b52503
   const [currentMovieIndex, setCurrentMovieIndex] = useState(1);
   const [movieData, setMovieData] = useState([]);
   // console.log(movies);
 
   useEffect(() => {
-      const controller = new AbortController();
-      fetch(`${baseUrl}apikey=${apikey}&i=${movies.Search[currentMovieIndex].imdbID}`, {signal: controller.signal})
+    const controller = new AbortController();
+    fetch(`${baseUrl}apikey=${apikey}&i=${movies.Search[currentMovieIndex].imdbID}`, { signal: controller.signal })
       .then(response => response.json())
       .then(data => setMovieData(data))
 
-      // console.log('use Effect');
+    // console.log('use Effect');
 
-      return () => {
-        controller.abort();
-      }
+    return () => {
+      controller.abort();
+    }
   }, [currentMovieIndex]);
 
-  useEffect(() =>{
+  useEffect(() => {
     console.log(movieData);
   }, [movieData])
 
@@ -37,10 +37,10 @@ function App() {
 
   return (
     <>
-      <h1 className='page-title'>Movie Reviews</h1>
+      {/* <h1 className='page-title'>Movie Reviews</h1> */}
       <div className="movies">
         <div className="box">
-          {movies.Search.map((movie, i) => <img key={i} className={`poster ${i === currentMovieIndex ? 'current-poster' : 'not-current-poster'}`} src={movie.Poster} onClick={() => setCurrentMovieIndex(i)}/>)}
+          {movies.Search.map((movie, i) => <img key={i} className={`poster ${i === currentMovieIndex ? 'current-poster' : 'not-current-poster'}`} src={movie.Poster} onClick={() => setCurrentMovieIndex(i)} />)}
         </div>
       </div>
 
@@ -55,7 +55,7 @@ function App() {
         <div className="reviews-content">
           <div className="movie-reviews-container">
             <div className="movie-reviews">
-              <h1 style={{margin: '1rem 0'}}>{movieData.Title} (Reviews)</h1>
+              <h1 style={{ margin: '1rem 0' }}>{movieData.Title} (Reviews)</h1>
               <div className="review"></div>
               <div className="review"></div>
               <div className="review"></div>
