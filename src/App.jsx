e7ecf6a54e9e88a1910a08d54a4e1e1fe6d0ef82
Nicholas Lee <nicholasleexyz@gmail.com>
2023-09-02 07:47:44 -0600

    component updates

--- src/App.jsx
+++ src/App.jsx
@@ -21,33 +21,65 @@ function Stars({isStatic, rating}) {
   )
 }
 
-function ReviewSubmissionForm({ title }) {
-  return(
-    <div className="review-submission-form">
-      {/* <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, perspiciatis minima dolorum ut velit veritatis doloribus voluptatum sit dolore odio saepe fugiat! Adipisci doloremque ad ducimus praesentium saepe perspiciatis pariatur!</p> */}
-      <div style={{display: "flex", height:"100%", width:"100%", justifyContent: "center", alignContent: "center", flexFlow: "column"}} className="">
-        <h2>Review of <strong>{title}</strong></h2>
-        <div style={{width : "100%", margin : "0.5rem 0"}} className="line-decor"></div>
-        <div className="">
-          <label style={{margin:"0 1rem 0 0"}} htmlFor="">Reviewer Name</label>
-          <input type="text" name="" id="" />
-        </div>
-        <div style={{display: "flex", backgroundColor: ""}} className="">
-          <label style={{margin: "auto 1rem auto 0"}} htmlFor="">Star Rating </label>
-          <Stars/>
-        </div>
-        <div style={{display: "flex", alignItems: "start"}}className="">
-          <label style={{margin:"0 1rem auto 0"}}htmlFor="">Comments</label>
-          <textarea name="" id="" ></textarea>
-        </div>
-        <div style={{width : "100%", margin : "0.5rem 0"}} className="line-decor">
+function ReviewSubmissionForm({ title, plot, currentMovieIndex  }) {
+  const [currentAverage, setCurrentAverage] = useState(0);
 
+  return (
+    <>
+      <div className="content">
+        <div className="info-content">
+          <div className="movie-info">
+            {/* Going to developer hell for this. Pretty sure they make you write PHP and use UML diagrams there. :P */}
+            <div className="asdf">
+              <div className="average-rating">
+                <span>Average Rating: </span><Stars isStatic={true} rating={currentAverage}/>
+              </div>
+              <div style={{margin: '0 1rem 0 1rem'}}className="">
+                <h2 style={{margin: '1rem auto 0.5rem auto'}} className='movie-info-title'>{title}</h2>
+                <p style={{maxWidth: "80ch"}}>{plot}</p>
+              </div>
+
+              <div className="line-decor"></div>
+
+              <div className="review-submission-form">
+                <div style={{display: "flex", height:"100%", width:"100%", justifyContent: "center", alignContent: "center", flexFlow: "column"}} className="">
+                  <h2>Review of <strong>{title}</strong></h2>
+                  <div style={{width : "100%", margin : "0.5rem 0"}} className="line-decor"></div>
+                  <div className="">
+                    <label style={{margin:"0 1rem 0 0"}} htmlFor="">Reviewer Name</label>
+                    <input type="text" name="" id="" />
+                  </div>
+                  <div style={{display: "flex", backgroundColor: ""}} className="">
+                    <label style={{margin: "auto 1rem auto 0"}} htmlFor="">Star Rating </label>
+                    <Stars/>
+                  </div>
+                  <div style={{display: "flex", alignItems: "start"}}className="">
+                    <label style={{margin:"0 1rem auto 0"}}htmlFor="">Comments</label>
+                    <textarea name="" id="" ></textarea>
+                  </div>
+                  <div style={{width : "100%", margin : "0.5rem 0"}} className="line-decor">
+
+                  </div>
+                  <div style={{alignSelf:"end", margin: "auto 0 0 0", height: "fit-content",width: "fit-content"}}className="">
+                    <button >Submit</button>
+                  </div>
+                </div>
+              </div>
+
+              {/* <ReviewSubmissionForm title={movieData.Title}/> */}
+            </div>
+          </div>
         </div>
-        <div style={{alignSelf:"end", margin: "auto 0 0 0", height: "fit-content",width: "fit-content"}}className="">
-          <button >Submit</button>
+
+        <div className="reviews-content">
+          <div className="movie-reviews-container">
+            <div className="movie-reviews">
+              <ReviewList title={title}/>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
@@ -91,51 +123,20 @@ function App() {
     }
   }, [currentMovieIndex]);
 
-  useEffect(() => {
-    console.log(movieData);
-  }, [movieData])
-
-  // console.log(currentMovieData);
-  // const plot = currentMovieData.Plot;
-  // const movieTitle = currentMovieData.Title;
-  // console.log(plot);
-
+  // useEffect(() => {
+  //   console.log(movieData);
+  // }, [movieData])
 
   return (
     <>
-      {/* <h1 className='page-title'>Movie Reviews</h1> */}
       <div className="movies">
         <div className="box">
           {movies.Search.map((movie, i) => <img key={i} className={`poster ${i === currentMovieIndex ? 'current-poster' : 'not-current-poster'}`} src={movie.Poster} onClick={() => setCurrentMovieIndex(i)} />)}
         </div>
       </div>
 
-      <div className="content">
-        <div className="info-content">
-          <div className="movie-info">
-            {/* Going to developer hell for this. Pretty sure they make you write PHP and use UML diagrams there. :P */}
-            <div className="asdf">
-              <div className="average-rating">
-                <span>Average Rating: </span><Stars/>
-              </div>
-              <div style={{margin: '0 1rem 0 1rem'}}className="">
-                <h2 style={{margin: '1rem auto 0.5rem auto'}} className='movie-info-title'>{movieData.Title}</h2>
-                <p style={{maxWidth: "80ch"}}>{movieData.Plot}</p>
-              </div>
-              <div className="line-decor"></div>
-              <ReviewSubmissionForm title={movieData.Title}/>
-            </div>
-          </div>
-        </div>
+      <ReviewSubmissionForm title={movieData.Title} plot={movieData.Plot} currentMovieIndex={currentMovieIndex}></ReviewSubmissionForm>
 
-        <div className="reviews-content">
-          <div className="movie-reviews-container">
-            <div className="movie-reviews">
-              <ReviewList title={movieData.Title}/>
-            </div>
-          </div>
-        </div>
-      </div>
     </>
   )
 }
