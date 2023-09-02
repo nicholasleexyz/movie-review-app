19b0f3e816d7bfe4b28af1ede4522cc3d35b4f24
Nicholas Lee <nicholasleexyz@gmail.com>
2023-09-02 07:12:23 -0600

    made some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { useEffect, useState } from 'react'
 import './App.css'
 
@@ -8,14 +9,65 @@ const movies = await fetch('/movies.json').then(response => response.json())
 
 // const currentMovieData = await getData();
 
-function Stars(){
-  const [rating, setRating] = useState(0);
+function Stars({isStatic, rating}) {
+  const [currentRating, setRating] = useState(rating);
 
   const starRange = [...Array(5).keys()];
 
   return (
     <div className="stars">
-      {starRange.map(i => <img key={i} src={i < rating ? '/star-solid.svg' : '/star-regular.svg'} className='star' onClick={() => setRating(i + 1)}/>)}
+      {starRange.map(i => <img key={i} src={i < currentRating ? '/star-solid.svg' : '/star-regular.svg'} className='star' onClick={isStatic ? null : () => setRating(i + 1)}/>)}
+    </div>
+  )
+}
+
+function ReviewSubmissionForm({ title }) {
+  return(
+    <div className="review-submission-form">
+      {/* <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, perspiciatis minima dolorum ut velit veritatis doloribus voluptatum sit dolore odio saepe fugiat! Adipisci doloremque ad ducimus praesentium saepe perspiciatis pariatur!</p> */}
+      <div style={{display: "flex", height:"100%", width:"100%", justifyContent: "center", alignContent: "center", flexFlow: "column"}} className="">
+        <h2>Review of <strong>{title}</strong></h2>
+        <div style={{width : "100%", margin : "0.5rem 0"}} className="line-decor"></div>
+        <div className="">
+          <label style={{margin:"0 1rem 0 0"}} htmlFor="">Reviewer Name</label>
+          <input type="text" name="" id="" />
+        </div>
+        <div style={{display: "flex", backgroundColor: ""}} className="">
+          <label style={{margin: "auto 1rem auto 0"}} htmlFor="">Star Rating </label>
+          <Stars/>
+        </div>
+        <div style={{display: "flex", alignItems: "start"}}className="">
+          <label style={{margin:"0 1rem auto 0"}}htmlFor="">Comments</label>
+          <textarea name="" id="" ></textarea>
+        </div>
+        <div style={{width : "100%", margin : "0.5rem 0"}} className="line-decor">
+
+        </div>
+        <div style={{alignSelf:"end", margin: "auto 0 0 0", height: "fit-content",width: "fit-content"}}className="">
+          <button >Submit</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function ReviewList({title}) {
+  return (
+    <>
+      <h1 style={{ margin: '1rem 0' }}>{title} (Reviews)</h1>
+      <Review rating={3}/>
+      <Review rating={1}/>
+      <Review rating={4}/>
+      <Review rating={2}/>
+      <Review rating={5}/>
+    </>
+  )
+}
+
+function Review({rating}) {
+  return (
+    <div className="review">
+      <Stars isStatic={true} rating={rating}/>
     </div>
   )
 }
@@ -61,7 +113,7 @@ function App() {
       <div className="content">
         <div className="info-content">
           <div className="movie-info">
-            {/* Going to developer hell for this. :P */}
+            {/* Going to developer hell for this. Pretty sure they make you write PHP and use UML diagrams there. :P */}
             <div className="asdf">
               <div className="average-rating">
                 <span>Average Rating: </span><Stars/>
@@ -71,32 +123,7 @@ function App() {
                 <p style={{maxWidth: "80ch"}}>{movieData.Plot}</p>
               </div>
               <div className="line-decor"></div>
-              <div className="review-submission-form">
-                {/* <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, perspiciatis minima dolorum ut velit veritatis doloribus voluptatum sit dolore odio saepe fugiat! Adipisci doloremque ad ducimus praesentium saepe perspiciatis pariatur!</p> */}
-                <div style={{display: "flex", height:"100%", width:"100%", justifyContent: "center", alignContent: "center", flexFlow: "column"}} className="">
-                  <h2>Review of <strong>{movieData.Title}</strong></h2>
-                  <div style={{width : "100%", margin : "0.5rem 0"}} className="line-decor"></div>
-                  <div className="">
-                    <label style={{margin:"0 1rem 0 0"}} htmlFor="">Reviewer Name</label>
-                    <input type="text" name="" id="" />
-                  </div>
-                  <div style={{display: "flex", backgroundColor: ""}} className="">
-                    <label style={{margin: "auto 1rem auto 0"}} htmlFor="">Star Rating </label>
-                    <Stars/>
-                  </div>
-                  <div style={{display: "flex", alignItems: "start"}}className="">
-                    <label style={{margin:"0 1rem auto 0"}}htmlFor="">Comments</label>
-                    <textarea name="" id="" ></textarea>
-                  </div>
-                  {/* <textarea name="" id="" ></textarea> */}
-                  <div style={{width : "100%", margin : "0.5rem 0"}} className="line-decor">
-
-                  </div>
-                  <div style={{alignSelf:"end", margin: "auto 0 0 0", height: "fit-content",width: "fit-content"}}className="">
-                    <button >Submit</button>
-                  </div>
-                </div>
-              </div>
+              <ReviewSubmissionForm title={movieData.Title}/>
             </div>
           </div>
         </div>
@@ -104,18 +131,7 @@ function App() {
         <div className="reviews-content">
           <div className="movie-reviews-container">
             <div className="movie-reviews">
-              <h1 style={{ margin: '1rem 0' }}>{movieData.Title} (Reviews)</h1>
-              <div className="review">
-                {/* star rating */}
-                <Stars/>
-                {/* <div className="star"></div> */}
-              </div>
-              <div className="review"></div>
-              <div className="review"></div>
-              <div className="review"></div>
-              <div className="review"></div>
-              <div className="review"></div>
-              <div className="review"></div>
+              <ReviewList title={movieData.Title}/>
             </div>
           </div>
         </div>
